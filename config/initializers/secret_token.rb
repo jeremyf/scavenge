7d6c8726c328f4534e1c0d7edaf669ccfeec2b99
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2011-06-14 09:23:20 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scavenge::Application.config.secret_token = '8ad3f49aa11af9a95ce78b4e529791edf905e23f44a10d73189a82248160c8741ca4d81d954891dbdc321dbe105f89a272bc75142eca22e19bb291bd3c3c9c40'
